ef2c40a7d5c70e6bee550b03a037ce253fd263a4
Kosarev Kirill <66671677+Kirushka-png@users.noreply.github.com>
2022-03-22 10:38:24 +0300

    added  some

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,27 @@ import Diagnostics from './components/pages/Diagnostics';
 import Diagnostic1 from './components/pages/Diagnostic1';
 import Diagnostic2 from './components/pages/Diagnostic2';
 import Diagnostic3 from './components/pages/Diagnostic3';
+import Footer from './components/defaultComponents/Footer'
+import Header from './components/defaultComponents/Header'
+import Login from './components/pages/Login/Login'
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 const App = () => {
   return (
-    <Diagnostic2/>
+    <BrowserRouter>
+      <Routes>
+          <Route path="login"><Login/></Route>
+          <Route path="main">
+            
+          </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
