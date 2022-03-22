3fef1b964114c5aac2a912fdd218bd202410bbfd
Kosarev Kirill <66671677+Kirushka-png@users.noreply.github.com>
2022-03-22 10:09:42 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Diagnostics from './components/pages/Diagnostics';
 import Diagnostic1 from './components/pages/Diagnostic1';
 import Diagnostic2 from './components/pages/Diagnostic2';
 import Diagnostic3 from './components/pages/Diagnostic3';
+
 const App = () => {
   return (
     <Diagnostic2/>
