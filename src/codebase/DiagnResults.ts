ffdcfc095ff5698462a33bd12d45f771086bcb96
Kosarev Kirill <66671677+Kirushka-png@users.noreply.github.com>
2022-04-01 14:01:42 +0300

    added results for 2 and 3

--- src/codebase/DiagnResults.ts
+++ src/codebase/DiagnResults.ts
@@ -3,11 +3,26 @@ export interface IDiagnResult{
     level: string
 }
 
+export interface IDiagnResult2 extends IDiagnResult{
+    description: string
+}
+const mockdataResult3 ={
+    titles : ['1 блок: Поведение','2 блок: Мышление','3 блок: Чувства','4 блок: Здоровье'],
+    levels: ['низкий уровень изменений', 'средний  уровень изменений','высокий уровень изменений']
+}
+const mockdataResult2 = {
+    titles : ['Копинг, ориентированный на решение задачи, проблемы','Копинг, ориентированный на эмоции','Копинг, ориентированный на избегание'],
+    power: ['Низкая степень выраженности','Средняя степень выраженности','Высокая степень выраженности'],
+    descriptions: ['Базируется на стараниях человека усовершенствовать взаимоотношения с внешней средой с помощью изменения когнитивной оценки конкретной ситуации и представляет собой намеренные проблемно-фокусированные усилия по изменению ситуации, включающие аналитический подход к решению проблемы.',
+    'Содержит в себе такие мысли и действия, целью которых является снижение физического или психологического давления стресса.',
+    'Характеризуется мысленным стремлением и поведенческими усилиями, направленными к бегству или избеганию проблемы.']
+}
 const mockdataResult1 ={
     titles : ['Эмоциональное истощение','Деперсонализация','Редукция личных достижений'],
     levels: ['Низкий уровень','Средний уровень','Высокий уровень','Очень высокий уровень'],
-    degrees: ['Низкая степень','Средняя степень','Высокая  степень', 'Крайне высокая степень']
+    power: ['Низкая степень','Средняя степень','Высокая степень', 'Крайне высокая степень']
 }
+
 export const AnswersIntoResultDiagn1 = (answers: number[]) => {
     let result = [0,0,0,0];
     let values:IDiagnResult[]  = [{title: '', level: ''},{title: '', level: ''},{title: '', level: ''},{title: '', level: ''}];
@@ -24,7 +39,7 @@ export const AnswersIntoResultDiagn1 = (answers: number[]) => {
     result[2] < 5 ? result[0] += 1 : result[2] < 13 ? result[0] += 2 : result[2] < 16 ? result[0] += 3 : result[0] += 5
     result[3] < 22 ? result[0] += 5 : result[3] < 28 ? result[0] += 3 : result[3] < 36 ? result[0] += 2 : result[0] += 1
     values[0].level = `${result[0]} балл(ов) - `
-    result[0] < 5 ? values[0].level += mockdataResult1.degrees[0] : result[0] < 7 ? values[0].level += mockdataResult1.degrees[1] : result[0] < 10 ? values[0].level += mockdataResult1.degrees[2] : values[0].level += mockdataResult1.degrees[3]
+    result[0] < 5 ? values[0].level += mockdataResult1.power[0] : result[0] < 7 ? values[0].level += mockdataResult1.power[1] : result[0] < 10 ? values[0].level += mockdataResult1.power[2] : values[0].level += mockdataResult1.power[3]
     values[0].title = 'Интегральный показатель выгорания:'
     values[1].title = mockdataResult1.titles[0]
     values[2].title = mockdataResult1.titles[1]
@@ -36,9 +51,37 @@ export const AnswersIntoResultDiagn1 = (answers: number[]) => {
 }
 
 export const AnswersIntoResultDiagn2 = (answers: number[]) => {
-    
+    let tempAnswers: number[] = answers.map((value) => value == 0 ? [1,0] : [0,1]).flat()
+    let result = [0,0,0];
+    let values:IDiagnResult2[]  = [{title: '', level: '',description: ''},{title: '', level: '',description: ''},{title: '', level: '',description: ''}];
+    [1,4,7,9,14,15,19,22].forEach(element => {
+        result[0] += tempAnswers[element-1]
+    });
+    [2,5,8,11,13,18,20,23].forEach(element => {
+        result[1] += tempAnswers[element-1]
+    });
+    [3,6,10,12,16,17,21,24].forEach(element => {
+        result[2] += tempAnswers[element-1]
+    });
+    values.forEach((element, index) => {
+        element.title = mockdataResult2.titles[index]
+        element.description = mockdataResult2.descriptions[index]
+        element.level = `${result[index]} балл(ов) - `
+        element.level += result[index] < 3 ? mockdataResult2.power[0] : result[index] < 6 ? mockdataResult2.power[1] :  mockdataResult2.power[2]
+    });
+    return values
 }
 
 export const AnswersIntoResultDiagn3 = (answers: number[]) => {
-    
+    let values:IDiagnResult[]  = [{title: '', level: ''},{title: '', level: ''},{title: '', level: ''}];
+    mockdataResult3.titles.forEach((element, index) => {
+        let tempResult = 0
+        answers.slice(index * 5, index * 5 + 5).forEach(element => {
+            tempResult += element
+        });
+        values[index].title = mockdataResult3.titles[index]
+        values[index].level = `${tempResult} балл(ов) - `
+        values[index].level += tempResult < 11 ? mockdataResult3.levels[0] : tempResult < 21 ? mockdataResult3.levels[1] :  mockdataResult3.levels[2]
+    });
+    return values
 }
