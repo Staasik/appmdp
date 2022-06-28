6bd5cd746d4a52a7bfa2efdae90b33e465fc838b
Kosarev Kirill <66671677+Kirushka-png@users.noreply.github.com>
2022-06-28 14:49:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,11 +11,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/createNewUser', (req, res) => {
-    console.log(req.body)
-})
 app.post('/acceptLogin', (req, res) => {
-    console.log(req.body)
     dbOperation.acceptLogin(req.body.login, req.body.password).then(r => {
         res.send(r)
     })
@@ -25,11 +21,7 @@ app.get('/getUsersData', (req, res) => {
         res.send(r)
     })
 })
-app.get('/get', (req, res) => {
-    res.send("Hello")
-})
 app.post('/registrationNewUser', (req, res) => {
-    console.log(req.body)
     dbOperation.registrationNewUser(req.body.name, req.body.login, req.body.password).then(r => {
         res.send(r)
     })
