f18108db4c5bfecc985ee7b8277eaef4d846a110
Kosarev Kirill <66671677+Kirushka-png@users.noreply.github.com>
2023-05-29 22:45:18 +0300

    AAAAA

--- server/dtos/answer.dto.js
+++ server/dtos/answer.dto.js
@@ -15,9 +15,11 @@ export class AnswerDTO {
 export class UserAnswerDTO {
     id
     text
+    value
 
     constructor(model) {
         this.id = model.id
         this.text = model.text
+        this.value = model.value
     }
 }
